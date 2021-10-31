f4c90ce12f1048ca977dd1aa3bb460cf528a8636
Maryem BEN-CHARKE <72795298+Maryem-bencharke@users.noreply.github.com>
2021-10-31 16:51:59 +0100

    Delete head.h

--- head.h
+++
@@ -1,9 +0,0 @@
-#ifndef EXERCICE_H
-#define EXERCICE_H
-# include <stdlib.h>
-# include <stdio.h>
-void check_msb(int );
-void print_bits(unsigned char );
-void print_nth_byte(int  , int );
-int **dummy(int **, int );
-#endif
